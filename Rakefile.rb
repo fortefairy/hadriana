eb678d85dda970922a8cc509500cd895ba7c031a
Alex Gil <colibri.alex@gmail.com>
2020-01-14 15:28:32 -0500

    fixed links in menu

---
+++ Rakefile.rb
@@ -0,0 +1,21 @@
+
+
+# Publishing the Website
+
+desc 'Publishing the website via rsync'
+
+
+task :prod do
+  # puts 'First, let\'s build your site...'
+  # sh "jekyll build"
+  # puts "\n"
+  puts 'Now let\'s publish it, hold on a sec...'
+# personal server setup
+  user = 'inallmyd'
+  server = 'inallmydreams.net'
+  path = '/home/inallmyd/public_html' 
+  sh "rsync -a -r -e \"ssh -p22\" _site/. #{user}@#{server}:#{path}"
+  puts "\n"
+  puts 'Bam! Your website is now published!'
+  puts "\n"
+end
