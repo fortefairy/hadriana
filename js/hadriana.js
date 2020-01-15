80b52a07d37bc69c347518d4ec66b6910bc3307f
Taylor <taylormfaires@gmail.com>
2020-01-15 13:12:57 -0500

    javascript

--- js/hadriana.js
+++ js/hadriana.js
@@ -5,18 +5,10 @@ $(document).ready(function() {
 
 // This hides the grid on load, then fades it in.
 
-    $('div.grid-layout').hide();
-	$('div.grid-layout').delay(2000).fadeIn(1000);
+    $('div.homepage').hide();
+	$('div.homepage').delay(2000).fadeIn(1000);
 
 });
 
-
-$(document).ready(function() {
-
-    // This hides the grid on load, then fades it in.
-
-        $('div.second-row').hide();
-        $('div.second-row').delay(2000).fadeIn(1000);
-    });
 
 
